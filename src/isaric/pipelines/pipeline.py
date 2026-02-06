c7915d62cf71a87a60c4885a105649af00f082ff
EnzoGCastro <enzoguerrieridecastro@gmail.com>
2026-02-06 09:24:39 -0300

    update pipeline to BasePipeline
    Changed RAPID_Pipeline to RAPID_BasePipeline to indicate that it is abstract and should not be implemented.
    
    Added comments to each method to indicate to contributors what should be implemented in a RAPID pipeline class.

--- src/isaric/pipelines/pipeline.py
+++ src/isaric/pipelines/pipeline.py
@@ -5,22 +5,33 @@ Designed to be inherited from by all other pipeline classes.
 
 from abc import ABC, abstractmethod
 
-class RAPID_Pipeline(ABC):
+class RAPID_BasePipeline(ABC):
+
+
+    @abstractmethod
+    def _preprocess_data():
+        """
+        Should call the _data_cleaning() and _preprocessing() methods.
+        """
+        pass
 
     # ------------------------------------------------------------------
     # PUBLIC METHODS
     # ------------------------------------------------------------------
 
     @abstractmethod
-    def preprocess_data():
-        pass
-
-    @abstractmethod
     def fit():
+        """
+        Should call _modeling(). 
+        Subsequently, should call _model_evaluation() and extract relevant assumptions tests and fit metrics.
+        """ 
         pass
 
     @abstractmethod
     def summary():
+        """
+        Should call visualizaiton().
+        """
         pass
 
     # ------------------------------------------------------------------
@@ -29,24 +40,47 @@ class RAPID_Pipeline(ABC):
 
     @abstractmethod
     def _data_cleaning():
+        """
+        Format the data such that can be utilized for the statistical model after the preprocessing stage. 
+        For instance, in a regresison, should remove null values.
+        """
         pass
     
     @abstractmethod
     def _preprocessing():
+        """
+        Should use the rapid_preprocess util to extract relevant matrices and generate
+        a formula for the model.
+        """
         pass
 
     @abstractmethod
     def _modeling():
+        """
+        Should fit the model and store it in self.fit_model()
+        """
         pass
 
     @abstractmethod
     def _model_evaluation():
+        """
+        If applicable, should check assumption tests.
+        Should evaluate fit/performance metrics. Should run cross validation if applicable. 
+        Performance metrics and assumption tests should each be stored in a separate dataframe.
+        """
         pass
 
     @abstractmethod
     def _validation():
+        """
+        Should provide a structure for comparing the metrics generated in the _model_evaluation() stage.
+        For multiple models stored in the same pipeline.
+        """
         pass
 
     @abstractmethod
     def _visualization():
+        """
+        Should optionally display assumption test results, performance metrics, as well as any and all relevant plots.
+        """
         pass
